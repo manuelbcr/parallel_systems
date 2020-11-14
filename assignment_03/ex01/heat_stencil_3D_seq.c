a77280aa69b2bc19f4363a8fe32299d28b5c8ebe
Philip Handl <philip.handl@gmail.com>
2020-11-14 07:24:27 +0100

    [ASSIGN3] adapt 3D matrix

--- assignment_03/ex01/heat_stencil_3D_seq.c
+++ assignment_03/ex01/heat_stencil_3D_seq.c
@@ -1,44 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 
 typedef double value_t;
 
-#define RESOLUTION 120
+#define RESOLUTION 100
 
-// -- vector utilities --
+// -- Matrix utilities --
 
-typedef value_t *Vector;
+typedef value_t ***Matrix;
 
-Vector createVector(int N);
+Matrix create3DMatrix(int N);
 
-void releaseVector(Vector m);
-
-void printTemperature(Vector m, int N);
+void releaseMatrix(Matrix m, int N);
 
 // -- simulation code ---
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv){
   // 'parsing' optional input parameter = problem size
-  int N = 2000;
+  int N = 200; // rows x columns
+
   if (argc > 1) {
     N = atoi(argv[1]);
   }
-  int T = N * 500;
-  printf("Computing heat-distribution for room size N=%d for T=%d timesteps\n", N, T);
+  int T = N * 100;
+  printf("Computing heat-distribution for room size N=%dx%dx%d, for T=%d timesteps\n", N, N, N, T);
 
   // ---------- setup ----------
 
   // create a buffer for storing temperature fields
-  Vector A = createVector(N);
-
-  // set up initial conditions in A
+  Matrix A = create3DMatrix(N);
+  releaseMatrix(A, N);
+  return 0;
+  /*// set up initial conditions in A
   for (int i = 0; i < N; i++) {
-    A[i] = 273; // temperature is 0° C everywhere (273 K)
+    for (int j = 0; j < N; j++) {
+      A[i][j] = 273; // temperature is 0° C everywhere (273 K)
+    }
   }
 
   // and there is a heat source in one corner
   int source_x = N / 4;
-  A[source_x] = 273 + 60;
+  int source_y = N / 8;
+  A[source_y][source_x] = 273 + 60;
 
   printf("Initial:\t");
   printTemperature(A, N);
@@ -47,109 +52,103 @@ int main(int argc, char **argv) {
   // ---------- compute ----------
 
   // create a second buffer for the computation
-  Vector B = createVector(N);
+  Matrix B = create3DMatrix(N);
 
   // for each time step ..
   for (int t = 0; t < T; t++) {
     // .. we propagate the temperature
     for (long long i = 0; i < N; i++) {
-      // center stays constant (the heat is still on)
-      if (i == source_x) {
-        B[i] = A[i];
-        continue;
+      for (long long j = 0; j < N; j++) {
+        // center stays constant (the heat is still on)
+        if (i == source_y && j == source_x) {
+          B[i][j] = A[i][j];
+          continue;
+        }
+
+        // get temperature at current position
+        value_t tc = A[i][j];
+
+        // get temperatures of adjacent cells
+        value_t tu = (j != 0) ? A[i][j - 1] : tc;
+        value_t ta = (j != N - 1) ? A[i][j + 1] : tc;
+        value_t tl = (i != 0) ? A[i - 1][j] : tc;
+        value_t tr = (i != N - 1) ? A[i + 1][j] : tc;
+
+        // compute new temperature at current position
+        B[i][j] = 0.25 * (tl + tr + tu + ta);
       }
-
-      // get temperature at current position
-      value_t tc = A[i];
-
-      // get temperatures of adjacent cells
-      value_t tl = (i != 0) ? A[i - 1] : tc;
-      value_t tr = (i != N - 1) ? A[i + 1] : tc;
-
-      // compute new temperature at current position
-      B[i] = tc + 0.2 * (tl + tr + (-2 * tc));
     }
 
     // swap matrices (just pointers, not content)
-    Vector H = A;
+    Matrix H = A;
     A = B;
     B = H;
 
     // show intermediate step
     if (!(t % 1000)) {
-      printf("Step t=%d:\t", t);
+      printf("Step t=%d:\n", t);
       printTemperature(A, N);
       printf("\n");
     }
   }
 
-  releaseVector(B);
+  releaseMatrix(B);
 
   // ---------- check ----------
-
-  printf("Final:\t\t");
+  printf("Final:\t\t\n");
   printTemperature(A, N);
   printf("\n");
 
+  FILE *fp;
+
+  fp = fopen("2D-output-seq.dat", "w");
+  fprintf(fp, "%d\n", N);
+
+
   int success = 1;
   for (long long i = 0; i < N; i++) {
-    value_t temp = A[i];
-    if (273 <= temp && temp <= 273 + 60)
-      continue;
-    success = 0;
-    break;
+    for (long long j = 0; j < N; j++) {
+      value_t temp = A[i][j];
+      fprintf(fp, "%f\n", temp);
+      if (273 <= temp && temp <= 273 + 60)
+        continue;
+      success = 0;
+      break;
+    }
   }
+
+  fclose(fp);
 
   printf("Verification: %s\n", (success) ? "OK" : "FAILED");
 
   // ---------- cleanup ----------
 
-  releaseVector(A);
+  releaseMatrix(A);
 
   // done
   return (success) ? EXIT_SUCCESS : EXIT_FAILURE;
+*/
 }
 
-Vector createVector(int N) {
-  // create data and index vector
-  return malloc(sizeof(value_t) * N);
-}
-
-void releaseVector(Vector m) { free(m); }
-
-void printTemperature(Vector m, int N) {
-  const char *colors = " .-:=+*^X#%@";
-  const int numColors = 12;
-
-  // boundaries for temperature (for simplicity hard-coded)
-  const value_t max = 273 + 30;
-  const value_t min = 273 + 0;
-
-  // set the 'render' resolution
-  int W = RESOLUTION;
-
-  // step size in each dimension
-  int sW = N / W;
-
-  // room
-  // left wall
-  printf("X");
-  // actual room
-  for (int i = 0; i < W; i++) {
-    // get max temperature in this tile
-    value_t max_t = 0;
-    for (int x = sW * i; x < sW * i + sW; x++) {
-      max_t = (max_t < m[x]) ? m[x] : max_t;
+Matrix create3DMatrix(int N) {
+  // create data and index Matrix
+  value_t ***mat = malloc(sizeof(value_t) * N);
+  for (int i = 0; i < N; i++) {
+    mat[i] = malloc(sizeof(value_t*) * N);
+    for (int j = 0; j < N; j++) {
+      mat[i][j] = malloc(sizeof(value_t) * N);
     }
-    value_t temp = max_t;
-
-    // pick the 'color'
-    int c = ((temp - min) / (max - min)) * numColors;
-    c = (c >= numColors) ? numColors - 1 : ((c < 0) ? 0 : c);
-
-    // print the average temperature
-    printf("%c", colors[c]);
   }
-  // right wall
-  printf("X");
+  return mat;
 }
+
+void releaseMatrix(Matrix m, int N) { 
+  for(int i=0; i < N; i++){
+    for(int j=0; j < N; j++){
+      free(m[i][j]);
+    }
+    free(m[i]); 
+  }
+  free(m);
+}
+
