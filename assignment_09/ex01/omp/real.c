14bb3eb1a8a467056ab7567efdf050327fef2a11
Davide De Sclavis <davide.de-sclavis@student.uibk.ac.at>
2021-01-17 08:52:18 +0100

    [ASSIGN9] potentially speed up of psinv()

--- assignment_09/ex01/omp/real.c
+++ assignment_09/ex01/omp/real.c
@@ -456,11 +456,14 @@ static void psinv(void *or, void *ou, int n1, int n2, int n3,
   double (*r)[n2][n1] = (double (*)[n2][n1])or;
   double (*u)[n2][n1] = (double (*)[n2][n1])ou;
 
+#pragma omp parallel
+{
   int i3, i2, i1;
 
   double r1[M], r2[M];
 
   if (timeron) timer_start(T_psinv);
+  #pragma omp for collapse(2)
   for (i3 = 1; i3 < n3-1; i3++) {
     for (i2 = 1; i2 < n2-1; i2++) {
       for (i1 = 0; i1 < n1; i1++) {
@@ -483,6 +486,8 @@ static void psinv(void *or, void *ou, int n1, int n2, int n3,
       }
     }
   }
+}
+
   if (timeron) timer_stop(T_psinv);
 
   //---------------------------------------------------------------------
